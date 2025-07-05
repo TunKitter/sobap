876fda44abf21b9c0d32e9021cd450cffd4aaadc
TunKitter <tunkitjava@gmail.com>
2025-07-05 15:22:21 +0700

    [routing]: variable route, get and post http method

--- Route.php
+++ Route.php
@@ -1,35 +1,38 @@
 <?php
 class Route
 {
-    private static array $route = ['get' => [], 'post' => [], '404' => null];
+    private static array $route = ['GET' => [], 'POST' => [], '404' => null];
     private static function checkExist($url, string $type)
     {
-        $data = self::$route[$type];
-        if (array_key_exists($url, $data))
-            return ['status' => true, 'data' => $data[$url]];
-        else
-            return ['status' => false, 'data' => null];
-
+        $url = trim(parse_url($url, PHP_URL_PATH), '/');
+        $matches = [];
+        $data = array_find(self::$route[$type], function ($k, $v) use (&$matches, $url) {
+            return preg_match("#^$v$#", $url, $matches);
+        });
+        array_shift($matches);
+        return ['status' => $data ? true : false, 'data' => $data ?? null, 'matches' => $matches];
     }
     public static function dispatch()
     {
-        $t = self::checkExist($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+        $t = self::checkExist($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
         if ($t['status'])
-            $t['data']();
+            $t['data']($t['matches']);
         else
             self::$route['404'] ? (self::$route['404'])() : print "Not found";
     }
     private static function addEndpoint(string $type, string $url, callable $callback)
     {
-        self::$route[$type][$url] = $callback;
+        $url = trim($url, '/');
+        $pattern = preg_replace("/\{[\w\-]+\}/", "([^\/]+)", $url);
+        self::$route[$type][$pattern] = $callback;
     }
     public static function get(string $url, callable $callback): void
     {
-        self::addEndpoint('get', $url, $callback);
+        self::addEndpoint('GET', $url, $callback);
     }
     public static function post(string $url, callable $callback): void
     {
-        self::addEndpoint('post', $url, $callback);
+        self::addEndpoint('POST', $url, $callback);
     }
     public static function handle404(callable $callback)
     {
--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 declare(strict_types=1);
 require "DataController.php";
 require "Route.php";
-Route::get("/demo", fn() => DataController::callMe());
-Route::get("/something", fn() => print "Hello something from get");
-Route::post("/something", fn() => print "Hello something but post");
+// Route::get("/demo", fn() => DataController::callMe());
+Route::get("/something/{post}", fn($data) => print_r($data));
+Route::get("/something/{post}/detail/{detail}", fn($data) => print_r($data));
+// Route::post("/something", fn() => print "Hello something but post");
 Route::handle404(function () {
     echo "Something went wrong bro";
 });
