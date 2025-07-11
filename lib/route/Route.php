38c9f545d0e4013fb0ecfedd071d6496ec009b9a
TunKitter <tunkitjava@gmail.com>
2025-07-11 08:27:51 +0700

    [test]: more test cases in route, [remove]: RouteHandler interface in Route object

--- lib/route/Route.php
+++ lib/route/Route.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "Request.php";
-require_once __DIR__ . DIRECTORY_SEPARATOR . "abstraction" . DIRECTORY_SEPARATOR . "RouteHandler.php";
+// require_once __DIR__ . DIRECTORY_SEPARATOR . "abstraction" . DIRECTORY_SEPARATOR . "RouteHandler.php";
 class Route
 {
     private static $instance;
