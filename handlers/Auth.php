e0a6e4f14ce02e685c2c568d32ac5152eedbe9a8
TunKitter <tunkitjava@gmail.com>
2025-07-12 10:20:14 +0700

    [validate]: alpha numeric validation, demo in auth login handlers

--- handlers/Auth.php
+++ handlers/Auth.php
@@ -8,6 +8,7 @@ class Auth
     public static function handleLogin(Request $request)
     {
         $view = View::getView('views/auth/login', ['methods/layout','methods/auth/login']);
+        if(Validate::from($request->post('username'))->alphaNumeric()->validate()['is_error']) exit("Invalid username");
         $view->login->setName("Your user name is : " . $request->post('username'). " and your password is : " . $request->post('password'));
         $view->layout->render();
     }
