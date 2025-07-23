c47ba421a8b7e9ccd0cb965c2b045903207e3ada
TunKitter <tunkitjava@gmail.com>
2025-07-23 13:57:03 +0700

    [feature]: update database feature, [database]: update MYSQL_DATABASE to demo, persistent connection and throw error if in dev

--- lib/database/Database.php
+++ lib/database/Database.php
@@ -20,8 +20,9 @@ class Database
             $db = getenv('MYSQL_DATABASE');
             $pass = getenv('MYSQL_PASSWORD');
             $port = getenv('MYSQL_PORT');
-            static::$db = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
-            static::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            static::$db = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass, [PDO::ATTR_PERSISTENT => true]);
+            if (getenv('MODE') == 'DEV')
+                static::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             static::$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         }
         return static::$db;
@@ -39,6 +40,18 @@ class Database
     {
         return Database::getInstance()->query("SELECT * FROM $table WHERE $where LIMIT 1")->fetch();
     }
+    public static function update(string $table, array $data, string $where)
+    {
+        if(count($data) == 0) return null;
+        $sql = "UPDATE $table SET ";
+        foreach ($data as $key => $value) {
+            $sql .= "$key = '$value', ";
+        }
+        $sql = substr($sql, 0, -2);
+        $sql .= " WHERE $where";
+        return Database::getInstance()->exec($sql);
+    }
 }
-$a = Database::getOne('orders', "OrderID = 10248");
+// $a = Database::getOne('something', "name='tunkit'");
+$a = Database::update('something', ['name' => 'tunkit'], "id=1");
 var_dump($a);
